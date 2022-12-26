0ec9dd5dabbdd544021c2d0b49820e8343d5fafb
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-12-26 20:15:30 +0200

    clean code

--- dopLab/src/main/java/com/example/smthing/controllers/SolveController.java
+++ dopLab/src/main/java/com/example/smthing/controllers/SolveController.java
@@ -13,8 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class SolveController {
     private final Table_equationWithSolution equationRepo;
