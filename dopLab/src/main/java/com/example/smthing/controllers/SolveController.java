f5af2c3a6c7260f36853094c53e1b47864c8b582
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-12-26 20:15:13 +0200

    Raise refactoring + clean code

--- dopLab/src/main/java/com/example/smthing/controllers/SolveController.java
+++ dopLab/src/main/java/com/example/smthing/controllers/SolveController.java
@@ -12,11 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.tags.Param;
-import org.springframework.web.servlet.tags.UrlTag;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 
 @Controller
 public class SolveController {
@@ -25,14 +22,13 @@ public class SolveController {
     private final String solutionAttrName = "SolutionSave";
 
 
-    //TODO при конкурентному використанні пожлива гонка
     public SolveController(Table_equationWithSolution equationRepo) {
         this.equationRepo = equationRepo;
     }
 
 
     @RequestMapping(value = "/solve", method = RequestMethod.POST)
-    public ModelAndView getInput(HttpServletRequest request, Model model, String a11, String a22, String a33, String a12, String a13, String a23, String a1, String a2, String a3, String a0) throws EquationIsNotASurfaceException, EquationNotNumberException, EquationNotEnoughCoefsException, EquationNullException {
+    public ModelAndView getInput(Model model, String a11, String a22, String a33, String a12, String a13, String a23, String a1, String a2, String a3, String a0) throws EquationIsNotASurfaceException, EquationNotNumberException, EquationNotEnoughCoefsException, EquationNullException {
         String[] aStr;
         String solution;
         aStr = new String[]{a11, a22, a33, a12, a13, a23, a1, a2, a3, a0};
@@ -42,42 +38,16 @@ public class SolveController {
         // TODO виключення можуть виникати не лише по причині накоректно заданої площини
         //  (також подивіться на https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc)
 
-        //model.put(aStrAttrName, aStr);
-        //model.put(solutionAttrName, solution);
-        request.getSession().setAttribute(aStrAttrName, aStr);
-        request.getSession().setAttribute(solutionAttrName, solution);
-        //System.out.println(Arrays.toString(aStr) +" " + solution + " " + model.size());
-        //aStr = (String[]) model.getAttribute(aStrAttrName);
-        //solution = (String) model.getAttribute(solutionAttrName);
-        //System.out.println(Arrays.toString(aStr) +" " + solution);
         ModelAndView modelAndView = new ModelAndView("solution");
         modelAndView.addObject(aStrAttrName, aStr);
         modelAndView.addObject(solutionAttrName, solution);
-//        modelAndView.addObject("fhgj", "fgh");
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!"+modelAndView.getModel());
-//        System.out.println(modelAndView.getView());
-//        System.out.println(modelAndView.getStatus());
-//        System.out.println(modelAndView.toString());
         modelAndView.addObject("action", "/solve/submitSolution");
         return modelAndView;
     }
 
     @RequestMapping(value = "/solve/submitSolution")
-    public String sumbmitSolution(HttpServletRequest request,
-                                  @RequestParam(name = aStrAttrName) String[] aStr1,
-                                  @RequestParam(name = solutionAttrName) String solution1
-//                                  @ModelAttribute(name = aStrAttrName) String[] aStr,
-//                                  @ModelAttribute(name = solutionAttrName) String solution1
-    ) {
-        String[] aStr;
-        String solution;
-
-        aStr = (String[]) request.getSession().getAttribute(aStrAttrName);
-        solution = (String) request.getSession().getAttribute(solutionAttrName);
-        System.out.println(request.toString());
-        System.out.println(request.getHttpServletMapping());
-        System.out.println(request.getQueryString());
-        System.out.println(Arrays.toString(aStr1) +" fg "+solution1);
+    public String sumbmitSolution(@RequestParam(name = aStrAttrName) String[] aStr,
+                                  @RequestParam(name = solutionAttrName) String solution) {
         equationWithSolution e = new equationWithSolution(aStr, solution);
         equationRepo.save(e);
         return "redirect:/";
