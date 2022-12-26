4b71d484d09ea7048a1cc735dcae1cf71e53c8ba
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-12-26 23:50:38 +0200

    change Interface name

--- dopLab/src/main/java/com/example/smthing/controllers/data/ITable_equationWithSolution.java
+++ dopLab/src/main/java/com/example/smthing/controllers/data/ITable_equationWithSolution.java
@@ -1,5 +1,5 @@
 package com.example.smthing.controllers.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Table_equationWithSolution extends JpaRepository<equationWithSolution, Integer> {
+public interface ITable_equationWithSolution extends JpaRepository<equationWithSolution, Integer> {
 }
